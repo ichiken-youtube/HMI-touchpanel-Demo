094dcd7339f29ae5a45257a0f8af0dd99202bedb
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-06-21 17:07:27 +0900

    サーバスレッド化

--- 03_RemoteCrawler/socket_server.py
+++ 03_RemoteCrawler/socket_server.py
@@ -1,6 +1,8 @@
 import cv2
 import socket
 import struct
+from threading import Thread
+import random
 
 PACKET_SIZE_LIMIT = 800
 FRAME_WIDTH =120
@@ -32,10 +34,9 @@ def split_image(grid_x, grid_y, grid_number, frame):
   return cv2.resize(grid_image, (FRAME_WIDTH, FRAME_HEIGHT))
 
 
-def send_frame(frame, client_socket):
+def send_frame(frame, grid, client_socket):
   image_bytes = frame.tobytes()
   size = len(image_bytes)
-  grid = 0
   # フレームサイズを送信
   header = struct.pack(">LLLL", FRAME_WIDTH, FRAME_HEIGHT, grid, size)
   print(FRAME_WIDTH, FRAME_HEIGHT, grid, size)
@@ -58,31 +59,25 @@ def shot(cap):
     exit()
   return frame
 
-def main():
+def main(client_socket):
   # Webカメラのキャプチャ
   cap = cv2.VideoCapture(0)
   if not cap.isOpened():
     print("カメラを開くことができませんでした。")
     exit()
-  # ソケット設定
-  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server_socket.bind(('0.0.0.0', 8000))
-  server_socket.listen(1)
-  print('サーバーはポート8000で待機中...')
-  
-  client_socket, client_address = server_socket.accept()
-  print(f'クライアント {client_address} と接続されました')
-  client_socket.settimeout(300)
   frame=shot(cap)
 
   try:
     while True:
       # クライアントからの送信要求を待つ
-      print('送信要求待ち')
+      #print('送信要求待ち')
       request = client_socket.recv(12)
       print(request)
       if request[:4] == b'RQST':
         grid = struct.unpack(">I",request[4:8])[0]
+        if(grid==0 or None):
+          grid = int(random.randint(1, GRID_X*GRID_Y))
+        print('grid:'+str(grid))
 
         #resized_frame = cv2.resize(frame, (FRAME_WIDTH, FRAME_HEIGHT))
         resized_frame = split_image(GRID_X, GRID_Y, grid, frame)
@@ -90,9 +85,10 @@ def main():
         # OpenCVのBGRからRGBに変換
         rgb_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2BGRA)
         # フレームを送信
-        send_frame(rgb_frame, client_socket)
+        send_frame(rgb_frame, grid, client_socket)
+        print('送信完了')
       elif request[:4] == b'SHOT':
-        print('撮影要求受諾')
+        #print('撮影要求受諾')
         params = struct.unpack(">II",request[4:12])
         GRID_X = params[0]
         GRID_Y = params[1]
@@ -105,7 +101,23 @@ def main():
   finally:
     cap.release()
     client_socket.close()
-    server_socket.close()
 
 if __name__ == '__main__':
-  main()
+  server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 5)
+  server_socket.bind(('0.0.0.0', 8000))
+  server_socket.listen(5)
+  try:
+    while True:
+      print("接続待機中...")
+      client_socket, client_address = server_socket.accept()
+      # ソケット設定
+      client_socket.settimeout(60)
+      print(f'クライアント {client_address} と接続されました')
+      thread = Thread(target = main,args=(client_socket,), daemon= True)
+      thread.start()
+  except KeyboardInterrupt:
+    print("\nサーバーを停止します")
+  finally:
+    server_socket.close()
+
