93fe9bc2b0d748d806dc2b5eb5f64cd596870cec
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-06-27 17:17:56 +0900

    8方向の移動を追加

--- 03_RemoteCrawler/motor.py
+++ 03_RemoteCrawler/motor.py
@@ -1,37 +1,88 @@
 import RPi.GPIO as GPIO
 import time
 
-PINS={'L_A':2, 'L_B':3 , 'R_A':4, 'R_B':17, 'SC_A':27, 'SC_B':22, 'PWM':18}
+PINS={'L_F':3, 'L_R':2 , 'R_F':17, 'R_R':4, 'SC_A':27, 'SC_B':22, 'PWM':18}
 
 GPIO.setmode(GPIO.BCM)
 
 for key in PINS.keys():
   GPIO.setup(PINS[key], GPIO.OUT)
+  GPIO.output(PINS[key], GPIO.LOW)
 PWM = GPIO.PWM ( PINS['PWM'], 50 )
 PWM.start(50)
 
 def motor(direction, power=50):
   PWM.ChangeDutyCycle( power )
   #進行方向はテンキーと同じ配列
-  # 0:前進, 1:後進, 2:左旋回, 3:右��回
-  if direction == 0:
-    GPIO.output(PINS['L_A'], GPIO.HIGH)
-    GPIO.output(PINS['L_B'], GPIO.LOW)
-    GPIO.output(PINS['R_A'], GPIO.HIGH)
-    GPIO.output(PINS['R_B'], GPIO.LOW)
-  elif direction == 1:
-    GPIO.output(PINS['L_A'], GPIO.LOW)
-    GPIO.output(PINS['L_B'], GPIO.HIGH)
-    GPIO.output(PINS['R_A'], GPIO.LOW)
-    GPIO.output(PINS['R_B'], GPIO.HIGH)
+  if direction == 1:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.HIGH)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  elif direction == 2:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.HIGH)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.HIGH)
+  elif direction == 3:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.HIGH)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  elif direction == 4:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.HIGH)
+    GPIO.output(PINS['R_F'], GPIO.HIGH)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  elif direction == 6:
+    GPIO.output(PINS['L_F'], GPIO.HIGH)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.HIGH)
+  elif direction == 7:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.HIGH)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  elif direction == 8:
+    GPIO.output(PINS['L_F'], GPIO.HIGH)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.HIGH)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  elif direction == 9:
+    GPIO.output(PINS['L_F'], GPIO.HIGH)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
+  else:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
 
 if __name__ == '__main__':
   try:
     while True:
-      for i in range(10):
-        motor(0)
-        time.sleep(1)
+      for i in range(5):
         motor(1)
         time.sleep(1)
+        motor(2)
+        time.sleep(1)
+        motor(3)
+        time.sleep(1)
+        motor(4)
+        time.sleep(1)
+        motor(6)
+        time.sleep(1)
+        motor(7)
+        time.sleep(1)
+        motor(8)
+        time.sleep(1)
+        motor(9)
+        time.sleep(1)
   except KeyboardInterrupt:
+    GPIO.output(PINS['L_F'], GPIO.LOW)
+    GPIO.output(PINS['L_R'], GPIO.LOW)
+    GPIO.output(PINS['R_F'], GPIO.LOW)
+    GPIO.output(PINS['R_R'], GPIO.LOW)
     GPIO.cleanup()
