953c06956112ed825903c3daa9ea88c79b021b88
CarbCraftBrewer <inquiry@ichiken-engineering.com>
2024-06-27 16:52:46 +0900

    前後繰り返す実験プログラム

--- 03_RemoteCrawler/motor.py
+++ 03_RemoteCrawler/motor.py
@@ -1,15 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-PNO = 2 
+PINS={'L_A':2, 'L_B':3 , 'R_A':4, 'R_B':17, 'SC_A':27, 'SC_B':22, 'PWM':18}
+PWR = GPIO.PWM ( PINS['PWM'], 50 )
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PNO, GPIO.OUT)
-try:
+
+for key in PINS.keys():
+  GPIO.setup(PINS[key], GPIO.OUT)
+
+def motor(direction, power=50):
+  PWR.ChangeDutyCycle( power )
+  #進行方向はテンキーと同じ配列
+  # 0:前進, 1:後進, 2:左旋回, 3:右��回
+  if direction == 0:
+    GPIO.output(PINS['L_A'], GPIO.HIGH)
+    GPIO.output(PINS['L_B'], GPIO.LOW)
+    GPIO.output(PINS['R_A'], GPIO.HIGH)
+    GPIO.output(PINS['R_B'], GPIO.LOW)
+  elif direction == 1:
+    GPIO.output(PINS['L_A'], GPIO.LOW)
+    GPIO.output(PINS['L_B'], GPIO.HIGH)
+    GPIO.output(PINS['R_A'], GPIO.LOW)
+    GPIO.output(PINS['R_B'], GPIO.HIGH)
+
+if __name__ == '__main__':
+  try:
     while True:
-        GPIO.output(PNO, GPIO.HIGH) # 点灯
+      for i in range(10):
+        motor(0)
         time.sleep(1)
-        GPIO.output(PNO, GPIO.LOW) # 消灯
+        motor(1)
         time.sleep(1)
-except KeyboardInterrupt:
+  except KeyboardInterrupt:
     GPIO.cleanup()
